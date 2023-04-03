9a9fa229c3b8880cdb93fdb761f883a44c7e76c2
ZKier <70117461+ZKier@users.noreply.github.com>
2023-04-03 11:47:20 -0400

    Added comments to help guide the assignment

--- project1.java
+++ project1.java
@@ -1,6 +1,35 @@
-import java.util.*;
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	Zacchaeus Kier &  Sofiat Okanlawon
+	
+	
+	
+	Purpose: Write a Java program to solve the following synchronization & deadlock problem.
+	
+	
+	
+ 	Problem: 
+ 		* Suppose a two-way (east-west), two-lane road contains a long one-lane bridge.
+ 	 
+ 		* A westbound (or eastbound) car can only use the bridge if there are no oncoming cars
+ 	  	  on the bridge.
+ 	  
+ 		* Because of accidents, a signaling system has been installed at each end of the bridge.
+ 	
+ 		* When a car approaches the bridge, a sensor notifies the controller computer by calling
+ 	      a function arrive with the car’s travel direction (east or west).
+ 	  
+		* When a car leaves the bridge, the sensor notifies the controller computer by calling
+	      passed with the car’s travel direction.
+	  
+		* The traffic controller sets the signal lights to allow or prevent cars wanting to pass
+	      the bridge.
+	  
+		* Construct and implement an algorithm for controlling the lights such that they
+	      synchronize the traffic to pass the bridge correctly. 
 
-//Zacchaeus Kier &  Sofiat Okanlawon
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.*;
 
 public class project1 {
 

1be49a998b9f7d9fdf5b3d66ac05a06da152299b
ZKier <70117461+ZKier@users.noreply.github.com>
2023-04-03 11:20:34 -0400

    Added project1.java file

---
+++ project1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+//Zacchaeus Kier &  Sofiat Okanlawon
+
+public class project1 {
+
+      static int[][] table = table_gen(15); 
+
+      public static void main(String[] args) {
+
+      }  
+
+
+      static class stats implements Runnable {
+      
+      int function;
+   
+      public stats(int num) {
+            function = num;
+      
+      }
+   
+      public void run() {
+            
+      }
+   
+   }
+
+}

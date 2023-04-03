2fd1df139fbcce1c26fce9639d12675ceac0dda6
ZKier <70117461+ZKier@users.noreply.github.com>
2023-04-03 12:09:22 -0400

    got some more bits added

--- project1.java
+++ project1.java
@@ -17,26 +17,34 @@
  		* Because of accidents, a signaling system has been installed at each end of the bridge.
  	
  		* When a car approaches the bridge, a sensor notifies the controller computer by calling
- 	      a function arrive with the car’s travel direction (east or west).
+ 	      a function (*arrive*) with the car’s travel direction (east or west).
  	  
 		* When a car leaves the bridge, the sensor notifies the controller computer by calling
-	      passed with the car’s travel direction.
+	      (*passed*) with the car’s travel direction.
 	  
 		* The traffic controller sets the signal lights to allow or prevent cars wanting to pass
 	      the bridge.
 	  
 		* Construct and implement an algorithm for controlling the lights such that they
 	      synchronize the traffic to pass the bridge correctly. 
+	      
+
+	You may also use Thread.sleep() with different sleep times for eastbound and westbound cars.    //so maybe Thread.sleep(random_time_between_1_to_4_secs) 
+	This would allow simulating different time taken to pass the bridge.
 
 *//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 import java.util.*;
 
 public class project1 {
 
-	static int[][] table = table_gen(15); 
-
 	public static void main(String[] args) {
-
+		
+		ArrayList<Integer> northBoundCars;
+		ArrayList<Integer> eastBoundCars;
+		
+		ArrayList<Integer> carOnBridge; //size should never be greater than one.
+		
+		ArrayList<Integer> carsPastBridge;
 	}  
 
 	

b7948bf668ccbd799be628155a83b4329b5d4db9
ZKier <70117461+ZKier@users.noreply.github.com>
2023-04-04 13:34:42 -0400

    updated the code

--- Project1OS/src/main.java
+++ Project1OS/src/main.java
@@ -2,8 +2,16 @@ import java.util.*;
 
 public class main {
 
+	int WEST_BOUND = 2;
+	int EAST_BOUND = 1;
+	
 	public static void main(String[] args) {
 		
+		System.out.println("Cars on bridge: not printed yet but imma do that");
+		
+		//semaphore object creation
+		//list creation
+		//create two threads
 		ArrayList<Integer> westBoundCars;
 		ArrayList<Integer> eastBoundCars;
 		

1f4087515071d3505083cc4ee0b2f1679179e62f
ZKier <70117461+ZKier@users.noreply.github.com>
2023-04-07 12:35:12 -0400

    made the code a bit more neat

--- Project1OS/src/Road.java
+++ Project1OS/src/Road.java
@@ -15,17 +15,7 @@ public class Road extends Thread {
 		this.direction = bound;
 		this.carsOnRoad = list;
 		this.s = s;
-		
-		
-		//this section may not matter****** not sure yet
-		if (direction == EAST_BOUND) {
-			//populate an odd number list
-		} else if (direction == WEST_BOUND) {
-			//populate an even number list
-		} else {
-			//close code and run an error
-		}
-		
+	
 	}
 	
 	//runs automatically when road is started.
@@ -37,9 +27,8 @@ public class Road extends Thread {
             
             try {
                 
-                //Critical Section
                 int arrayLength = this.carsOnRoad.size();
-                
+                //works its way down the list of cars on the road
                 for (int i = 0; i < arrayLength; i++) {
                 	
                 	int carNum = this.carsOnRoad.get(i);
@@ -47,6 +36,8 @@ public class Road extends Thread {
                 		
                 		//Imagine this to be the green light that starts the West Bound's Red Light
                         s.acquire();
+                        
+                        //Critical Section
                         System.out.println("East bound light turns: GREEN");
                 		System.out.println("East --> bound vehicle: " + carNum + " is arriving");
                 		
@@ -55,38 +46,30 @@ public class Road extends Thread {
                 		Thread.sleep(sleepTime);
                 		System.out.println("East --> bound vehicle: " + carNum + " has passed\n");
                 		
-                		//should let another thread get the chance to acquire but this never happens... the threads just run concurrently.
+                		//gives other threads the chance to make their lights green
                 		s.release();
-                		//this.notifyAll();
-                		//this.wait();
-                		
-                		
-                		sleepTime = (int) ((Math.random() * 2000) + 750);
-                		Thread.sleep(sleepTime);
                 		
                 	} else {
                 		continue;
                 	}
-                    //sharedCounter.counter++;
-                    //System.out.println(threadName + " current value: " + sharedCounter.counter); 
                 	
-                    // Now incrementer thread will sleep so that the decrementer thread 
-                    //(if a context switch happen) get a chance to update
-                    //Thread.sleep(SLEEP_TIME); //change to 100, and then 1000
                 }
+                
             } catch (InterruptedException e) {
-                    System.out.println(e);
+            	
+            	System.out.println(e);
+                    
             }
             
-            //prof added this in class
+            //releases the lock so that other threads can work and finish
             s.release();
 
         } else {
+        	
         	System.out.println("Starting West Bound thread...");
             
             try {
                 
-                //Critical Section
                 int arrayLength = this.carsOnRoad.size();
                 
                 for (int i = 0; i < arrayLength; i++) {
@@ -94,8 +77,9 @@ public class Road extends Thread {
                 	int carNum = this.carsOnRoad.get(i);
                 	
                 	if ((carNum % 2) == 0) {
-                		//Imagine this to be the green light that starts the West Bound's Red Light
+                		//Imagine this to be the green light that starts the East Bound's Red Light
                 		s.acquire();
+                		//Critical Section
                 		System.out.println("West Bound light turns: GREEN");
                 		System.out.println("West <-- bound vehicle: " + carNum + " is arriving");
                 		
@@ -104,10 +88,6 @@ public class Road extends Thread {
                 		Thread.sleep(sleepTime);
                 		System.out.println("West <-- bound vehicle: " + carNum + " has passed\n");
                 		s.release();
-
-                		
-                		//sleepTime = (int) ((Math.random() * 2000) + 750);
-                		//Thread.sleep(sleepTime);
                 		
                 	} else {
                 		
@@ -116,8 +96,11 @@ public class Road extends Thread {
                 	}
 
                 }
+                
             } catch (InterruptedException e) {
-                    System.out.println(e);
+                   
+            	System.out.println(e);
+            	
             }
             
             //to make sure the other threads can finish
--- Project1OS/src/project1.java
+++ Project1OS/src/project1.java
@@ -10,6 +10,7 @@ public class project1 {
 		
 		ArrayList<Integer> cars = new ArrayList<>();
 		
+		//allows for the randomization of the length of the car array (randomizes the amount of cars on the road)
 		int randomLen = (int) ((Math.random() * 12) + 2);
 		
 		//makes a list of 2-12 cars, each car having a number between 0-100
@@ -21,14 +22,17 @@ public class project1 {
 			cars.add(i, randomCarNum);
 		}
 		
+		//prints the list of cars for reference
 		System.out.println(cars);
 		
+		//creates semaphore object
 		Semaphore S = new Semaphore(1, true);
 		
+		//create the roads
 		Road eastBound = new Road(EAST_BOUND, cars, S);
 		Road westBound = new Road(WEST_BOUND, cars, S);
 		
-		
+		//start and join the threads
 		eastBound.start();		westBound.start();
 
 		eastBound.join();		westBound.join();
